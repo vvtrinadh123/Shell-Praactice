c33d928a0072cd04b8c34240d6a01887285499aa
vvtrinadh123 <trinadhvenkat.v@gmail.com>
2026-05-26 17:49:32 +0530

    special vars

--- special-vars.sh
+++ special-vars.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-##### Special Vars####
+##### Special Vars #####
 echo "All variables passed to script: $@"
 echo "Number of variables passed: $#"
-echo "first variable: $1"
+echo "First variable: $1"
 echo "Script name: $0"
-echo "who is running this: $USER "
+echo "Who is running this: $USER"
+echo "Which directory: $PWD"
+echo "Home directory: $HOME"
+echo "PID of the current script: $$"
+sleep 5 &
+echo "PID of the background command running just now: $!"
+wait $!
+echo "Line number: $LINENO"
+echo "Script executed in $SECONDS seconds"
+echo "Random number: $RANDOM"
+echo "Exit code of previous command: $?"
