9ec5d04b791034929baacba967289b7a448919dc
vvtrinadh123 <trinadhvenkat.v@gmail.com>
2026-05-26 17:41:58 +0530

    special vars

--- special-vars.sh
+++ special-vars.sh
@@ -3,3 +3,6 @@
 ##### Special Vars####
 echo "All variables passed to script: $@"
 echo "Number of variables passed: $#"
+echo "first variable: $1"
+echo "Script name: $0"
+echo "who is running this: $USER "
